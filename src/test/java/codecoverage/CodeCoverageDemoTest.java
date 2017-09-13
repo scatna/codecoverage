c3a0283429dfbbd10e285cd74a22bdfab2824d2f
scatna <scatna@gmail.com>
2017-09-13 15:07:55 -0500

    remove cube junit test case

--- src/test/java/codecoverage/CodeCoverageDemoTest.java
+++ src/test/java/codecoverage/CodeCoverageDemoTest.java
@@ -15,11 +15,5 @@ public class CodeCoverageDemoTest {
 		assertEquals(25,a);
 		
 	}
-/*	@Test
-	public void testCube() {
-		CodeCoverageDemo demo = new CodeCoverageDemo();
-		int a = demo.cube(5);
-		assertEquals(125,a);
-		
-	}*/
+
 }
